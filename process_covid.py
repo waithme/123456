9a7371f13027111c1e03efdbf8576bc26b5c32f9
waithme <55341604+waithme@users.noreply.github.com>
2020-12-04 16:12:25 +0800

    test

---
+++ process_covid.py
@@ -0,0 +1,48 @@
+# FIXME add needed imports
+
+def load_covid_data(filepath):
+    raise NotImplementedError
+
+def cases_per_population_by_age(input_data):
+    raise NotImplementedError
+
+def hospital_vs_confirmed(input_data):
+    raise NotImplementedError
+
+def generate_data_plot_confirmed(input_data, sex, max_age, status):
+    """
+    At most one of sex or max_age allowed at a time.
+    sex: only 'male' or 'female'
+    max_age: sums all bins below this value, including the one it is in.
+    status: 'new' or 'total' (default: 'total')
+    """
+    raise NotImplementedError
+
+def create_confirmed_plot(input_data, sex=False, max_ages=[], status=..., save=...):
+    # FIXME check that only sex or age is specified.
+    fig = plt.figure(figsize=(10, 10))
+    # FIXME change logic so this runs only when the sex plot is required
+    for sex in ['male', 'female']:
+        # FIXME need to change `changeme` so it uses generate_data_plot_confirmed
+        plt.plot('date', 'value', changeme)
+    # FIXME change logic so this runs only when the age plot is required
+    for age in max_ages:
+        # FIXME need to change `changeme` so it uses generate_data_plot_confirmed
+        plt.plot('date', 'value', changeme)
+    fig.autofmt_xdate()  # To show dates nicely
+    # TODO add title with "Confirmed cases in ..."
+    # TODO Add x label to inform they are dates
+    # TODO Add y label to inform they are number of cases
+    # TODO Add legend
+    # TODO Change logic to show or save it into a '{region_name}_evolution_cases_{type}.png'
+    #      where type may be sex or age
+    plt.show()
+
+def compute_running_average(data, window):
+    raise NotImplementedError
+
+def simple_derivative(data):
+    raise NotImplementedError
+
+def count_high_rain_low_tests_days(input_data):
+    raise NotImplementedError
